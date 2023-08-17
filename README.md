9dcc5bbde5d3d32d1826fb0a19d0f12f762a267e
adamerikoff <adamerikoff@protonmail.com>
2023-08-17 22:03:09 +0600

    insert item to storage added

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # sandyq
  storage management system | C language
 
-SELECT storages.name, items.name, quantity FROM storage_item INNER JOIN items ON storage_item.item_id = items.id INNER JOIN storages ON storage_item.storage_id = storages.id;
--- main.c
+++ main.c
@@ -3,19 +3,8 @@
 
 #include <sqlite3.h>
 
-
-#define MENUMAX 5
-
-// PRINTS MENU
-void print_main_menu(char *menu[MENUMAX]){
-    for (int i = 0; i < MENUMAX; i++){
-        printf("%s\n", menu[i]);
-    }
-}
-
 // PRINTS STORAGES
 void print_storages(){
-    system("clear");  
     sqlite3 *db;
     char *errMsg = 0;
 
@@ -42,9 +31,39 @@ void print_storages(){
         sqlite3_close(db);
     }
 }
+
+void print_storage_items(int storage_id){
+    sqlite3 *db;
+    char *errMsg = 0;
+
+    int rc = sqlite3_open("sandyq.db", &db);
+    
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        const char *sql = "SELECT storages.name, items.name, quantity FROM storage_item INNER JOIN items ON storage_item.item_id = items.id INNER JOIN storages ON storage_item.storage_id = storages.id WHERE storages.id = ?";
+        
+        sqlite3_stmt *stmt;
+
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        sqlite3_bind_int(stmt, 1, storage_id);
+        printf("\e[1m%-15s %-15s %-15s\e[m\n", "Storage Name", "Item Name", "Quantity");
+        while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+            const unsigned char *storage_name = sqlite3_column_text(stmt, 0);
+            const unsigned char *item_name = sqlite3_column_text(stmt, 1);
+            int item_quantity = sqlite3_column_int(stmt, 2);
+
+            printf("%-15s %-15s %-5d\n", storage_name, item_name, item_quantity);
+        }
+
+        sqlite3_finalize(stmt);
+
+        sqlite3_close(db);
+    }
+}
+
 // PRINTS ITEMS
 void print_items(){
-    system("clear");  
     sqlite3 *db;
     char *errMsg = 0;
 
@@ -92,6 +111,99 @@ void insert_item_to_items(char inputString[100]){
 
             rc = sqlite3_step(stmt);
             if (rc != SQLITE_DONE) {
+                //fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
+            } else {
+                //printf("Item has been inserted successfully.\n");
+            }
+        }
+        sqlite3_finalize(stmt);
+    }
+    sqlite3_close(db);
+}
+
+// GET ID OF THE ITEM
+int get_id_of_item(char inputString[100]){
+    int productID;
+
+    sqlite3 *db;
+    char *errMsg = 0;
+
+    int rc = sqlite3_open("sandyq.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+
+    const char *sql = "SELECT id FROM items WHERE name = ?";
+    sqlite3_stmt *stmt;
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+
+    sqlite3_bind_text(stmt, 1, inputString, -1, SQLITE_STATIC);
+
+    if ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+        productID = sqlite3_column_int(stmt, 0);
+    } else {
+        return 0;
+    }
+
+    sqlite3_finalize(stmt);
+
+    sqlite3_close(db);
+
+    return productID;
+}
+
+void insert_item_to_storage(){
+    char inputString[100];
+    int storage;
+    int quantity;
+    int item_id;
+
+    printf("Enter item's name: ");
+    scanf("%s", inputString);
+    
+    insert_item_to_items(inputString);
+
+    printf("Enter item's quantity: ");
+    scanf("%d", &quantity);
+
+    print_storages();
+
+    printf("Select storage: ");
+    scanf("%d", &storage);
+
+    item_id = get_id_of_item(inputString);
+
+    //printf("%d %s %d %d", item_id, inputString, quantity, storage);
+    // INSERT NEW ITEM INTO storage_item table
+
+    sqlite3 *db;
+    char *errMsg = 0;
+
+    int rc = sqlite3_open("sandyq.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    } else {
+        const char *sql = "INSERT INTO storage_item (storage_id, item_id, quantity) VALUES (?, ?, ?)";
+        sqlite3_stmt *stmt;
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+        } else {
+            sqlite3_bind_int(stmt, 1, storage);
+            sqlite3_bind_int(stmt, 2, item_id);
+            sqlite3_bind_int(stmt, 3, quantity);
+
+            rc = sqlite3_step(stmt);
+            if (rc != SQLITE_DONE) {
                 fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
             } else {
                 printf("Item has been inserted successfully.\n");
@@ -102,23 +214,6 @@ void insert_item_to_items(char inputString[100]){
     sqlite3_close(db);
 }
 
-void add_item_to_storage(){
-    char inputString[100];
-    int storage;
-    int quantity;
-
-    printf("Enter item's name: ");
-    scanf("%s", inputString);
-
-    printf("Enter item's quantity: ");
-    scanf("%d", &quantity);
-
-    print_storages();
-
-    printf("Select storage: ");
-    scanf("%d", &storage);
-
-}
 
 
 
@@ -127,17 +222,52 @@ void add_item_to_storage(){
 // MAIN FUNCTION
 int main(void)
 {
-    char *main_menu[MENUMAX] = {
-        "1. show storage",
-        "2. add item",
-        "3. remove item",
-        "4. update item",
-        "5. exit"
-    };
+    int choice = 0;
+    int storage_choice = 0;
 
-    print_items();
-    insert_item_to_items("fanta 1l");
-    print_items();
+    do {
+        printf("1. show storages\n");
+        printf("2. show items\n");
+        printf("3. add item\n");
+        printf("4. remove item\n");
+        printf("5. update item\n");
+        printf("6. exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
+        switch (choice)
+        {
+            case 1:
+                print_storages();
+                getchar();
+                break;
+            
+            case 2:
+                print_storages();
+                printf("Enter your choice: ");
+                scanf("%d", &storage_choice);
+                print_storage_items(storage_choice);
+                break;
+
+            case 3:
+                insert_item_to_storage();
+                break;
+            
+            case 4:
+                printf("updated");
+                getchar();
+                break;
+            
+            case 5:
+                break;
+            
+            case 6:
+                break;
+
+            default:
+                printf("Wrong choice!\n");
+                break;
+        }
+    } while (choice != 6);
     return 0;
 }
