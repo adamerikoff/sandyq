ada3a14ee89dd0b3f654d61ac67c03435319d1d7
adamerikoff <adamerikoff@protonmail.com>
2023-08-17 17:12:06 +0600

    added print_function()

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sandyq
  storage management system | C language
+
+SELECT storages.name, items.name, quantity FROM storage_item INNER JOIN items ON storage_item.item_id = items.id INNER JOIN storages ON storage_item.storage_id = storages.id;
--- main.c
+++ main.c
@@ -11,9 +11,40 @@ void print_menu(char *menu[MENUMAX]){
     }
 }
 
+void print_storages(){
+    sqlite3 *db;
+    char *errMsg = 0;
+
+    // Open a database connection (or create if it doesn't exist)
+    int rc = sqlite3_open("sandyq.db", &db);
+    
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    }
+    else{
+        const char *sql = "SELECT storages.id, storages.name FROM storages";
+        sqlite3_stmt *stmt;
+
+        rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+        printf("\e[1m%-5s %s\e[m\n", "ID", "Storage Name");
+        while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+            const unsigned char *id = sqlite3_column_text(stmt, 0);
+            const unsigned char *name = sqlite3_column_text(stmt, 1);
+
+            printf("%-5s %s\n", id, name);
+        }
+
+        sqlite3_finalize(stmt);
+
+        sqlite3_close(db);
+    }
+
+    
+}
+
 int main(void)
 {
-
     char *main_menu[MENUMAX] = {
         "1. show storage",
         "2. add item",
@@ -22,13 +53,20 @@ int main(void)
         "5. exit"
     };
 
-    print_menu(main_menu);
+    //print_menu(main_menu);
 
-    char choice;
+    // char inputString[100];
+    // int storage;
 
+    // printf("Enter a string: ");
+    // scanf("%s\n", inputString);
+
+    // printf("Select storage: ");
+    // scanf("%d\n", &storage);
+
+    // printf("You entered: %s and %d", inputString, storage);
     
-    choice = getchar();
-    printf("%c\n", choice);
-    
+    print_storages();
+
     return 0;
 }
