57365d627747693f1467ba27c3bfc4d005419cad
Adam Erikuly <adamerikoff@protonmail.com>
2024-05-11 15:42:31 +0500

    simple tracker functionality added

--- tracker_ts/src/tracker.ts
+++ tracker_ts/src/tracker.ts
@@ -1,3 +1,48 @@
-(() => {
-    console.log("testing!")
-})();
+interface TrackingData {
+    type: "event" | "page";
+    identity: string;
+    ua: string;
+    event: string;
+    category: string;
+    referrer: string;
+}
+
+interface TrackPayload {
+    tracking: TrackingData;
+    site_id: string;
+}
+
+class Tracker {
+    url: string;
+    
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    track(event: string, category: string) {
+        const payload: TrackPayload = {
+            tracking: {
+                type: "event",
+                event: "temp-data",
+                identity: "temp-data",
+                ua: "temp-data",
+                category: "temp-data",
+                referrer: "temp-data",
+            },
+            site_id: "temp-site-id",
+        };
+        this.trackRequest(payload)
+    }
+
+    private trackRequest(payload: TrackPayload) {
+        const json_payload = JSON.stringify(payload);
+        const url_string = `${this.url}?data=${btoa(json_payload)}`
+        const img = new Image();
+        img.src = url_string;
+    }
+}
+
+((window, document) => {
+    window._tracker = new Tracker();
+    console.log("tracker loaded!");
+})(window, document);
